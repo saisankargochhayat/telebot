b743f40f357db946456fe4f556cfed171e17e9dc
Sai Sankar Gochhayat <saisankargochhayat@gmail.com>
2018-06-07 13:49:36 +0530

    Initial telebot

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # telebot
 Telegram bot to mass message users.
+
+Steps to setup the project- 
+Go to project directory - 
+pip3 install telethon
+(make sure you have python3 and pip3 installed.)
+
+Now that you have telethon installed on your debian system-
+ 1. Go to this link - https://my.telegram.org/apps and make a client. For this step you need a working phone number and a telegram application. The app title and shortname can be anything of your choice. 
+ 2. Once you are done with this step copy the api_id and api_hash value. 
+ 3. Make a copy of 'settings.example' and rename it to 'settings'. With the message parameter containing the message you want to send to your clients. 
+ 4. Make a copy of 'contacts.csv.example' and rename it to 'contacts.csv' and add the numbers of clients whom you want to send message in telegram in interational format. (example in contacts.csv.example).
+ 5. Now run 'python3 telebot.py'
+Voilla you have sucessfully sent all your clients the specific message. 
---
+++ telebot.py
@@ -0,0 +1,38 @@
+from telethon import TelegramClient
+
+# These example values won't work. You must get your own api_id and
+# api_hash from https://my.telegram.org, under API Development.
+result = {}
+path="settings"
+with open(path, 'r', encoding='utf-8') as file:
+    for line in file:
+        value_pair = line.split('=')
+        left = value_pair[0].strip()
+        right = value_pair[1].strip()
+        if right.isnumeric():
+            result[left] = int(right)
+        else:
+            result[left] = right
+print(result)
+
+api_id = result['api_id'] 
+api_hash = result['api_hash'] 
+
+client = TelegramClient('session_name', api_id, api_hash)
+client.start()
+print("Connected to client.")
+# Client message initialized
+message = result['message']
+
+with open("contacts.csv") as fileobject:
+    for line in fileobject:
+        print('Sending message to '+line)
+        client.send_message(line, message)
+print("All the messages have been sent sucessfully.")
+#print(client.get_me().stringify())
+
+# client.send_file('username', '/home/myself/Pictures/holidays.jpg')
+
+# client.download_profile_photo('me')
+# messages = client.get_messages('+917000000000')
+# client.download_media(messages[0])
